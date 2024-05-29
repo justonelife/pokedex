251b980ae93af3a83212e09f4b98d7209fb8438d
nguyennhut <nhut.nguyen@xomad.com>
2024-05-29 17:31:35 +0700

    feat: add lazyload img directive

--- libs/core/src/directives/index.ts
+++ libs/core/src/directives/index.ts
@@ -1,1 +1,2 @@
 export * from './lib-spinner.directive';
+export * from './lazy-load-image.directive';
---
+++ libs/core/src/directives/lazy-load-image.directive.ts
@@ -0,0 +1,64 @@
+import { DestroyRef, Directive, ElementRef, inject, Input, OnInit } from "@angular/core";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { distinctUntilChanged, Subject, tap } from "rxjs";
+
+@Directive({
+  selector: '[lazySrc]',
+  standalone: true,
+})
+export class LazyLoadImageDirective implements OnInit {
+  private readonly destroy = inject(DestroyRef);
+  private readonly elementRef = inject(ElementRef);
+
+  @Input({ alias: 'lazySrc', required: true }) imgSrc!: string;
+  @Input() rootMargin?: string;
+
+  private observer!: IntersectionObserver;
+  private observer$: Subject<HTMLElement> = new Subject<HTMLElement>();
+
+  ngOnInit(): void {
+    this.setup();
+    this.watch();
+  }
+
+  private setup(): void {
+    const options: IntersectionObserverInit = {
+      rootMargin: this.rootMargin,
+    };
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.observer$.next(entry.target as HTMLElement);
+        }
+      });
+    }, options);
+    this.observer.observe(this.elementRef.nativeElement);
+  }
+
+  private watch() {
+    this.observer$
+      .asObservable()
+      .pipe(
+        distinctUntilChanged(),
+        tap((value) => {
+          this.setImage(value as any, this.imgSrc);
+        }),
+        takeUntilDestroyed(this.destroy)
+      )
+      .subscribe();
+  }
+
+  private setImage(element: HTMLDivElement | HTMLImageElement, imageSrc: string) {
+    if (!this.isImageElement(element)) {
+      element.style.background = `url(${imageSrc})`;
+      return;
+    }
+    if (element instanceof HTMLImageElement) {
+      element.src = imageSrc;
+    }
+  }
+
+  private isImageElement(element: HTMLElement) {
+    return element.nodeName.toLowerCase() === 'img';
+  }
+}
